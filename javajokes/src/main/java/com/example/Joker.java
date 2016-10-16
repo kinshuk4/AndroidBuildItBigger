1bad269d8017b714787b04a8db69732d68399e9c
Kinshuk Chandra <kinshuk.ram@gmail.com>
2016-10-16 07:24:26 +0530

    Adding the java library to tell the jokes.

---
+++ javajokes/src/main/java/com/example/Joker.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Joker {
+
+    private static String[] JOKES = {
+            "Instagram is just Twitter for people who go outside.",
+    };
+
+    public String tellJoke(){
+        return JOKES[0];
+    }
+}
