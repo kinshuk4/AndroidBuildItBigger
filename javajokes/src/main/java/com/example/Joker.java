615bff550e8375f48954493751ec9c9d30162bc4
Kinshuk Chandra <kinshuk.ram@gmail.com>
2016-10-17 00:29:35 +0530

    added more jokes

--- javajokes/src/main/java/com/example/Joker.java
+++ javajokes/src/main/java/com/example/Joker.java
@@ -1,12 +1,18 @@
 package com.example;
 
-public class Joker {
+import java.util.Random;
 
+public class Joker {
+    private static final Random random = new Random();
     private static String[] JOKES = {
-            "Instagram is just Twitter for people who go outside.",
+            "Strong people don't put others down. They lift them up and slam them on the ground for maximum damage.",
+            "I think my neighbor is stalking me as she's been googling my name on her computer. I saw it through my telescope last night.",
+            "I can totally keep secrets. It's the people I tell them to that can't.",
+            "Whatever you do always give 100 %. Unless you are donating blood.",
+
     };
 
     public static String tellJoke(){
-        return JOKES[0];
+        return JOKES[random.nextInt(JOKES.length)];
     }
 }
